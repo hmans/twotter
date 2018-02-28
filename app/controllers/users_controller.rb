20174ba94e3a984ab65accbffaf2064f4a795c12
Hendrik Mans <hendrik@mans.de>
2018-02-28 16:23:09 +0000

    User registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,20 @@
 class UsersController < ApplicationController
-  before_action :load_user, except: [:show]
-  before_action :require_user, except: [:show]
+  before_action :load_user, except: [:new, :create, :show]
+  before_action :require_user, except: [:new, :create, :show]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login_user(@user)
+      redirect_to :timeline, notice: "Welcome to TWOTTER!"
+    else
+      render :new
+    end
+  end
 
   def show
     @user = User.includes(:posts).find(params[:id])
@@ -22,5 +36,11 @@ class UsersController < ApplicationController
 
   def load_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params
+      .require(:user)
+      .permit(%w[name full_name email password password_confirmation])
   end
 end
