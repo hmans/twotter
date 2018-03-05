321b3a5392a182654e6e85056d7f0ecdb6d550d5
Hendrik Mans <hendrik@mans.de>
2018-03-05 11:59:46 +0000

    Add a model spec for Post

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'when saving' do
+    let(:body) { 'a potentially filthy body' }
+    let(:filtered_body) { 'filtered body' }
+    subject(:post) { build :post, user: create(:user), body: body }
+
+    it 'invokes the PoopFilter to filter filth from the user input' do
+      # Set up a spy
+      allow(PoopFilter)
+        .to receive(:filter)
+        .and_return(filtered_body)
+
+      # Save the post
+      post.save!
+
+      # Was the filter invoked?
+      expect(PoopFilter)
+        .to have_received(:filter)
+        .with(body)
+
+      # Was the value on the post set correctly?
+      expect(post.body).to eq(filtered_body)
+    end
+  end
 end
