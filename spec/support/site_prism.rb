1df66cfada4c52e159f13f56cb27e601389c0c66
Hendrik Mans <hendrik@mans.de>
2018-03-06 09:55:40 +0000

    Use inheritance for SitePrism pages

--- spec/support/site_prism.rb
+++ spec/support/site_prism.rb
@@ -3,8 +3,15 @@ module PageObjects
     elements :posts, 'article.post'
   end
 
-  class GlobalPage < SitePrism::Page
-    set_url '/global'
+  class PageWithPost < SitePrism::Page
     section :posts_section, PostsSection, 'section.posts'
+  end
+
+  class GlobalPage < PageWithPost
+    set_url '/global'
+  end
+
+  class TimelinePage < PageWithPost
+    set_url '/timeline'
   end
 end
