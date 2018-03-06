b18fd6f1d9b33949f7647bf206fcc64f0d1f991e
Hendrik Mans <hendrik@mans.de>
2018-03-06 10:36:18 +0000

    Don't override Capybara's page helper

--- spec/system/global_post_list_with_site_prism_spec.rb
+++ spec/system/global_post_list_with_site_prism_spec.rb
@@ -10,23 +10,23 @@ describe 'global post page', js: true do
   let!(:alices_post) { create(:post, user: alice, body: "Hi, I'm Alice!") }
   let!(:bobs_post) { create(:post, user: bob, body: "And I'm Bob!") }
 
-  subject(:page) { PageObjects::GlobalPage.new }
+  subject(:global_page) { PageObjects::GlobalPage.new }
 
   specify 'it lists all posts by all users' do
-    page.load
+    global_page.load
 
-    expect(page).to have_posts_section
+    expect(global_page).to have_posts_section
 
-    expect(page.posts_section).to have_posts(count: 2)
+    expect(global_page.posts_section).to have_posts(count: 2)
 
     # Bob's post is the newest, so it should be first...
-    page.posts_section.posts.first.tap do |post|
+    global_page.posts_section.posts.first.tap do |post|
       expect(post.body).to have_text(bobs_post.body)
       expect(post.meta).to have_text(bob.full_name)
     end
 
     # ...followed by Alice's post.
-    page.posts_section.posts.second.tap do |post|
+    global_page.posts_section.posts.second.tap do |post|
       expect(post.body).to have_text(alices_post.body)
       expect(post.meta).to have_text(alice.full_name)
     end
